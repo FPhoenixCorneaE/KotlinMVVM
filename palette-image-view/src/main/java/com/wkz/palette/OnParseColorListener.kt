f84ed9d2475f53eb9c87a0dcd33e54636bd69337
汪坤柱 <wangkz@galanz.com>
2020-05-09 16:59:22 +0800

    feat:PaletteImageView;

---
+++ palette-image-view/src/main/java/com/wkz/palette/OnParseColorListener.kt
@@ -0,0 +1,16 @@
+package com.wkz.palette
+
+/**
+ * @desc 解析图片颜色监听器
+ */
+interface OnParseColorListener {
+    /**
+     * 解析完成
+     */
+    fun onComplete(paletteImageView: PaletteImageView?)
+
+    /**
+     * 解析失败
+     */
+    fun onFail()
+}
