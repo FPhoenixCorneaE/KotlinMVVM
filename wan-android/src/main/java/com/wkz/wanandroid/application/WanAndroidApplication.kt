5a746f970a0173add8e7f1d56f9f6dba5da00edd
汪坤柱 <wangkz@galanz.com>
2020-05-09 17:25:39 +0800

    feat:适配Android 10以上深色模式主题;

--- wan-android/src/main/java/com/wkz/wanandroid/application/WanAndroidApplication.kt
+++ wan-android/src/main/java/com/wkz/wanandroid/application/WanAndroidApplication.kt
@@ -1,10 +1,13 @@
 package com.wkz.wanandroid.application
 
+import androidx.appcompat.app.AppCompatDelegate
 import com.wkz.framework.base.BaseApplication
 
 class WanAndroidApplication : BaseApplication() {
 
     override fun onCreate() {
         super.onCreate()
+        // 初始化Night mode,设置Theme随系统变化,应用的Theme继承DayNight Theme
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
     }
 }
