2d889f58c57d01e010ab6e3a758410c9b29918ce
wkz <wangkz@galanz.com>
2020-09-12 15:20:15 +0800

    build:远程依赖BottomNavigationView;

--- buildSrc/src/main/groovy/Config.groovy
+++ buildSrc/src/main/groovy/Config.groovy
@@ -11,11 +11,12 @@ class Config {
 
     /* 组件 */
     static FPhoenixCorneaE = [
-            FlowLayout    : "com.github.FPhoenixCorneaE:FlowLayout:1.0.6",
-            ShapeImageView: "com.github.FPhoenixCorneaE:ShapeImageView:1.0.5",
-            PrettyDialog  : "com.github.FPhoenixCorneaE:PrettyDialog:1.0.1",
-            AvatarWaveView: 'com.github.FPhoenixCorneaE:AvatarWaveView:1.0.0',
-            EasyBehavior  : 'com.github.FPhoenixCorneaE:EasyBehavior:1.0.1',
+            FlowLayout          : "com.github.FPhoenixCorneaE:FlowLayout:1.0.6",
+            ShapeImageView      : "com.github.FPhoenixCorneaE:ShapeImageView:1.0.5",
+            PrettyDialog        : "com.github.FPhoenixCorneaE:PrettyDialog:1.0.1",
+            AvatarWaveView      : 'com.github.FPhoenixCorneaE:AvatarWaveView:1.0.0',
+            EasyBehavior        : 'com.github.FPhoenixCorneaE:EasyBehavior:1.0.1',
+            BottomNavigationView: 'com.github.FPhoenixCorneaE:BottomNavigationView:1.0.3',
     ]
 
     /** Android */
