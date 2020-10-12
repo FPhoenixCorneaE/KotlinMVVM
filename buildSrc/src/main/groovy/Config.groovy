ddc877d002af64974946b0a2550450ddb6e4de8f
wkz <wangkz@galanz.com>
2020-10-12 16:22:01 +0800

    build:远程依赖:通用工具类CommonUtil-1.0.1;

--- buildSrc/src/main/groovy/Config.groovy
+++ buildSrc/src/main/groovy/Config.groovy
@@ -11,6 +11,7 @@ class Config {
 
     /* 组件 */
     static FPhoenixCorneaE = [
+            CommonUtil            : "com.github.FPhoenixCorneaE:CommonUtil:1.0.1",
             FlowLayout            : "com.github.FPhoenixCorneaE:FlowLayout:1.0.6",
             ShapeImageView        : "com.github.FPhoenixCorneaE:ShapeImageView:1.0.5",
             PrettyDialog          : "com.github.FPhoenixCorneaE:PrettyDialog:1.0.1",
